c88f5a72333025e2f32b893d5efe8c531fe5a354
Craig Watson <craig@bashton.com>
2016-08-25 12:04:43 +0100

    Initial commit

---
+++ 10-vars.tf
@@ -0,0 +1,8 @@
+variable "prefix" {}
+variable "name" {}
+variable "subnets" {}
+variable "security_groups" {}
+variable "r53_zone_id" { default = "" }
+variable "r53_domain" { default = "" }
+variable "create_cnames" { default = 0 }
+variable "availability_zones" { default = "" }
---
+++ 20-main.tf
@@ -0,0 +1,48 @@
+resource "aws_efs_file_system" "fs" {
+  tags { Name = "${var.prefix}-${var.name}" }
+}
+
+resource "aws_efs_mount_target" "mount_a" {
+  file_system_id  = "${aws_efs_file_system.fs.id}"
+  subnet_id       = "${element(split(",",var.subnets),0)}"
+  security_groups = ["${split(",", var.security_groups)}"]
+}
+
+resource "aws_efs_mount_target" "mount_b" {
+  file_system_id  = "${aws_efs_file_system.fs.id}"
+  subnet_id       = "${element(split(",",var.subnets),1)}"
+  security_groups = ["${split(",", var.security_groups)}"]
+}
+
+resource "aws_efs_mount_target" "mount_c" {
+  file_system_id  = "${aws_efs_file_system.fs.id}"
+  subnet_id       = "${element(split(",",var.subnets),2)}"
+  security_groups = ["${split(",", var.security_groups)}"]
+}
+
+resource "aws_route53_record" "cname_a" {
+  count   = "${var.create_cnames}"
+  zone_id = "${var.r53_zone_id}"
+  name    = "efs-${var.name}-${element(split(",",var.availability_zones),0)}.${var.r53_domain}"
+  type    = "CNAME"
+  ttl     = "86400"
+  records = [ "${aws_efs_mount_target.mount_a.dns_name}" ]
+}
+
+resource "aws_route53_record" "cname_b" {
+  count   = "${var.create_cnames}"
+  zone_id = "${var.r53_zone_id}"
+  name    = "efs-${var.name}-${element(split(",",var.availability_zones),1)}.${var.r53_domain}"
+  type    = "CNAME"
+  ttl     = "86400"
+  records = [ "${aws_efs_mount_target.mount_b.dns_name}" ]
+}
+
+resource "aws_route53_record" "cname_c" {
+  count   = "${var.create_cnames}"
+  zone_id = "${var.r53_zone_id}"
+  name    = "efs-${var.name}-${element(split(",",var.availability_zones),2)}.${var.r53_domain}"
+  type    = "CNAME"
+  ttl     = "86400"
+  records = [ "${aws_efs_mount_target.mount_c.dns_name}" ]
+}
